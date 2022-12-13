45eb269736e9a93e46896d45644b9c9976bec69d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-13 18:24:03 -0500

    Modify attributes to resemble New York Times cell

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -5,11 +5,9 @@ const Cell = (props) => {
 				x={props.x}
 				y={props.y}
 				tabIndex={0}
-				width="32"
-				height="32"
-				fill="white"
-				stroke="black"
-				strokeWidth="1"></rect>
+				width="33"
+				height="33"
+				fill="white"></rect>
 		</g>
 	);
 };
