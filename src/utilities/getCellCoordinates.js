d96e3c9c835d9b27ee6835fa1acadd6d398f3dc8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-13 13:32:51 -0500

    Fix method to set cells in their correct positions
    Changed constants so they are generalizable to the x and y coordinates.
    Also changed for loop variables to x and y so their purpose is clearer

--- src/utilities/getCellCoordinates.js
+++ src/utilities/getCellCoordinates.js
@@ -1,11 +1,11 @@
 export const getCellCoordinates = (cellsPerRow) => {
 	let array = [];
-	const CELL_Width = 32;
-	const lastCellStart = cellsPerRow * CELL_Width - CELL_Width;
+	const CELL_LENGTH = 32;
+	const GRID_LENGTH = cellsPerRow * CELL_LENGTH;
 
-	for (let i = 0; i <= cellsPerRow; i++) {
-		for (let j = 0; j < lastCellStart; j += CELL_Width) {
-			array.push({ x: j, y: i });
+	for (let y = 0; y < GRID_LENGTH; y += CELL_LENGTH) {
+		for (let x = 0; x < GRID_LENGTH; x += CELL_LENGTH) {
+			array.push({ x: x, y: y });
 		}
 	}
 	return array;
