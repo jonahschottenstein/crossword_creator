583d7472b9654c6fecfb0a69537406326be815c7
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-13 16:05:07 -0500

    Add Clue component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import "../App.css";
 import Cell from "./Cell";
 import { getCellCoordinates } from "../utilities/getCellCoordinates.js";
+import { Clue } from "./Clue.js";
 
 class App extends Component {
 	constructor(props) {
@@ -13,12 +14,15 @@ class App extends Component {
 			return <Cell key={`cell-${index + 1}`} x={coord.x} y={coord.y} />;
 		});
 		return (
-			<section className="xword-board-section">
-				<div className="xword-board-container">
-					<svg className="xword-board" width="100%" height="100%">
-						<g>{cells}</g>
-					</svg>
-				</div>
+			<section className="xword-clue-and-board-section">
+				<Clue />
+				<section className="xword-board-section">
+					<div className="xword-board-container">
+						<svg className="xword-board" width="100%" height="100%">
+							<g>{cells}</g>
+						</svg>
+					</div>
+				</section>
 			</section>
 		);
 	}
