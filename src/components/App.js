bb195b89f3e8323f9231292feb0c8579bb7d291d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-13 13:42:32 -0500

    Render cells to form grid

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../App.css";
 import Cell from "./Cell";
+import { getCellCoordinates } from "../utilities/getCellCoordinates.js";
 
 class App extends Component {
 	constructor(props) {
@@ -8,12 +9,13 @@ class App extends Component {
 	}
 	state = {};
 	render() {
+		const cells = getCellCoordinates(15).map((coord, index) => {
+			return <Cell key={`cell-${index + 1}`} x={coord.x} y={coord.y} />;
+		});
 		return (
 			<div className="svg-container">
 				<svg width="100%" height="100%">
-					<g>
-						<Cell />
-					</g>
+					<g>{cells}</g>
 				</svg>
 			</div>
 		);
