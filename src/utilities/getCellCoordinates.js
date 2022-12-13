ae98726e9a996c8a60ca8e3f4664c44da7999b37
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-13 18:25:15 -0500

    Update cell length and initial x/y values
    Initial x and y values are set to 3 to account for the width of the grid
    border

--- src/utilities/getCellCoordinates.js
+++ src/utilities/getCellCoordinates.js
@@ -1,10 +1,10 @@
 export const getCellCoordinates = (cellsPerRow) => {
 	let array = [];
-	const CELL_LENGTH = 32;
+	const CELL_LENGTH = 33;
 	const GRID_LENGTH = cellsPerRow * CELL_LENGTH;
 
-	for (let y = 0; y < GRID_LENGTH; y += CELL_LENGTH) {
-		for (let x = 0; x < GRID_LENGTH; x += CELL_LENGTH) {
+	for (let y = 3; y < GRID_LENGTH; y += CELL_LENGTH) {
+		for (let x = 3; x < GRID_LENGTH; x += CELL_LENGTH) {
 			array.push({ x: x, y: y });
 		}
 	}
