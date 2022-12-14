52e65af3af3b469a6d331f5321fe24aa41668dbb
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-14 02:29:49 -0500

    Add method to toggle cell block of symmetrical cell

---
+++ src/utilities/toggleCellBlockSymmetry.js
@@ -0,0 +1,22 @@
+export const toggleCellBlockSymmetry = (e) => {
+	const cellBlockInput = document.querySelector("#cell-block-toggle-input");
+	const symmetryInput = document.querySelector("#cell-block-symmetry-input");
+	const cellRects = document.querySelectorAll(".cell-rect");
+
+	if (!cellBlockInput.checked) return;
+	if (!symmetryInput.checked) return;
+	if (!e.target.matches("rect")) return;
+
+	const numberOfCells = cellRects.length;
+	const lastCellIndex = numberOfCells - 1;
+	const targetIndex = e.target.getAttribute("data-index");
+	const symmetricalIndex = lastCellIndex - targetIndex;
+	const centerCellIndex = Math.floor(numberOfCells / 2);
+
+	if (!targetIndex !== centerCellIndex) {
+		console.log(targetIndex);
+		console.log(cellRects);
+		console.log(cellRects[symmetricalIndex]);
+		cellRects[symmetricalIndex].classList.toggle("cell-block");
+	}
+};
