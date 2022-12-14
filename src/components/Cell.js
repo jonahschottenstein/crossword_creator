7bdc1ea7e9dfdc8205e954536aceefb6f5b02bb4
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-13 19:22:52 -0500

    Add offset values to x/y of rect and text elements

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,13 +1,36 @@
 const Cell = (props) => {
+	const RECT_OFFSET_X = 3;
+	const RECT_OFFSET_Y = 3;
+
+	const NUMBER_OFFSET_X = 5;
+	const NUMBER_OFFSET_Y = 14.5;
+
+	const LETTER_OFFSET_X = 19.5;
+	const LETTER_OFFSET_Y = 33.25;
+
 	return (
 		<g className="xword-cell">
 			<rect
-				x={props.x}
-				y={props.y}
+				x={props.x + RECT_OFFSET_X}
+				y={props.y + RECT_OFFSET_Y}
 				tabIndex={0}
 				width="33"
 				height="33"
 				fill="white"></rect>
+			<text
+				x={props.x + NUMBER_OFFSET_X}
+				y={props.y + NUMBER_OFFSET_Y}
+				textAnchor="start"
+				fontSize="11">
+				1
+			</text>
+			<text
+				x={props.x + LETTER_OFFSET_X}
+				y={props.y + LETTER_OFFSET_Y}
+				textAnchor="middle"
+				fontSize="22">
+				A
+			</text>
 		</g>
 	);
 };
