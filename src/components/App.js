611885670eac939c299f08561a8f05b8361be364
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-14 13:50:15 -0500

    Import Checkbox component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import { getCellCoordinates } from "../utilities/getCellCoordinates.js";
 import { Clue } from "./Clue.js";
 import { toggleCellBlock } from "../utilities/toggleCellBlock.js";
 import { toggleCellBlockSymmetry } from "../utilities/toggleCellBlockSymmetry.js";
+import { Checkbox } from "./Checkbox.js";
 
 class App extends Component {
 	constructor(props) {
