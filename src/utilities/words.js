9e0a6f655874e5c9f7eb5b7f6c21c046665ed5e5
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-18 19:39:46 -0500

    Add words.js
    This file will hold functions that are used to get across/down words

---
+++ src/utilities/words.js
@@ -0,0 +1,102 @@
+import {
+	getCellIndex,
+	cellsInSameColumn,
+	cellInFirstRow,
+	cellInFirstColumn,
+	cellInLastRow,
+	cellInLastColumn,
+} from "./cellLocation.js";
+
+import {
+	previousAcrossCellBlocked,
+	previousDownCellBlocked,
+	nextAcrossCellBlocked,
+	nextDownCellBlocked,
+} from "./cellBlockLocation.js";
+
+export const getAcrossFirstLetterCells = (numberedCells) => {
+	const firstLetterCells = numberedCells.filter(
+		(numberedCell) =>
+			cellInFirstColumn(numberedCell) || previousAcrossCellBlocked(numberedCell)
+	);
+	return firstLetterCells;
+};
+
+export const getDownFirstLetterCells = (numberedCells) => {
+	const firstLetterCells = numberedCells.filter(
+		(numberedCell) =>
+			cellInFirstRow(numberedCell) || previousDownCellBlocked(numberedCell)
+	);
+	return firstLetterCells;
+};
+
+export const getAcrossLastLetterCells = (firstLetterCells) => {
+	const cellsArray = [...document.querySelectorAll(".xword-cell")];
+	const lastLetterCells = firstLetterCells.map((firstLetterCell) => {
+		return cellsArray.find((cell, index) => {
+			return (
+				index > getCellIndex(firstLetterCell) &&
+				(cellInLastColumn(cell) || nextAcrossCellBlocked(cell))
+			);
+		});
+	});
+	return lastLetterCells;
+};
+
+export const getDownLastLetterCells = (firstLetterCells) => {
+	const cellsArray = [...document.querySelectorAll(".xword-cell")];
+	const lastLetterCells = firstLetterCells.map((firstLetterCell) => {
+		return cellsArray.find((cell, index, array) => {
+			return (
+				index > getCellIndex(firstLetterCell) &&
+				cellsInSameColumn(cell, firstLetterCell) &&
+				(cellInLastRow(cell) || nextDownCellBlocked(cell))
+			);
+		});
+	});
+
+	return lastLetterCells;
+};
+
+export const getAcrossWords = (firstLetterCells, lastLetterCells) => {
+	const cellsArray = [...document.querySelectorAll(".xword-cell")];
+	const firstAndLastLetterCells = firstLetterCells.map(
+		(firstLetterCell, firstLetterCellIndex, firstLetterCellArray) => ({
+			firstLetterCell: firstLetterCellArray[firstLetterCellIndex],
+			lastLetterCell: lastLetterCells[firstLetterCellIndex],
+		})
+	);
+
+	const wordsArray = firstAndLastLetterCells.map(
+		({ firstLetterCell, lastLetterCell }) =>
+			cellsArray.filter(
+				(cell, index) =>
+					index >= getCellIndex(firstLetterCell) &&
+					index <= getCellIndex(lastLetterCell)
+			)
+	);
+
+	return wordsArray;
+};
+
+export const getDownWords = (firstLetterCells, lastLetterCells) => {
+	const cellsArray = [...document.querySelectorAll(".xword-cell")];
+	const firstAndLastLetterCells = firstLetterCells.map(
+		(firstLetterCell, firstLetterCellIndex, firstLetterCellArray) => ({
+			firstLetterCell: firstLetterCellArray[firstLetterCellIndex],
+			lastLetterCell: lastLetterCells[firstLetterCellIndex],
+		})
+	);
+
+	const wordsArray = firstAndLastLetterCells.map(
+		({ firstLetterCell, lastLetterCell }) =>
+			cellsArray.filter(
+				(cell, index) =>
+					index >= getCellIndex(firstLetterCell) &&
+					index <= getCellIndex(lastLetterCell) &&
+					cellsInSameColumn(cell, firstLetterCell)
+			)
+	);
+
+	return wordsArray;
+};
