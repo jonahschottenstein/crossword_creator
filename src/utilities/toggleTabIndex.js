49a0c4c90577b8598e9b0801205be99fd2fdbba1
Jonah Schottenstein <jonahschottenstein@gmail.com>
2022-12-18 21:55:03 -0500

    Fix method to set symmetrical cell block tabIndex

--- src/utilities/toggleTabIndex.js
+++ src/utilities/toggleTabIndex.js
@@ -1,5 +1,16 @@
-export const toggleTabIndex = (e) => {
+/* export const toggleTabIndex = (e) => {
 	e.target.classList.contains("cell-block")
 		? (e.target.tabIndex = -1)
 		: (e.target.tabIndex = 0);
+}; */
+
+export const setTabIndex = () => {
+	const rectsArray = document.querySelectorAll(".cell-rect");
+	rectsArray.forEach((rect) => {
+		if (rect.classList.contains("cell-block")) {
+			rect.tabIndex = -1;
+		} else {
+			rect.tabIndex = 0;
+		}
+	});
 };
