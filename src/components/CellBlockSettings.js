e7018d76949436c42efaaee5e8ba9b6cbcc075ba
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-02 18:53:48 -0500

    Nest ToggleSwitch components in CellBlockSettings

--- src/components/CellBlockSettings.js
+++ src/components/CellBlockSettings.js
@@ -1,3 +1,22 @@
+import { ToggleSwitch } from "./ToggleSwitch.js";
+
 export const CellBlockSettings = (props) => {
-	return <div className="cell-block-settings"></div>;
+	return (
+		<div className="cell-block-settings">
+			<ToggleSwitch
+				toggleSwitchId="cell-block-input"
+				text="Cell Block"
+				name="cellBlockInput"
+				isChecked={props.cellBlockInput}
+				onChange={props.onChange}
+			/>
+			<ToggleSwitch
+				toggleSwitchId="symmetry-input"
+				text="Symmetry"
+				name="symmetryInput"
+				isChecked={props.symmetryInput}
+				onChange={props.onChange}
+			/>
+		</div>
+	);
 };
