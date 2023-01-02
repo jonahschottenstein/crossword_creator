1e9ea9dd62f6f34fe9ed8b1978049be9af49a457
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-02 17:36:05 -0500

    Render Cell components in Board component

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,3 +1,20 @@
+import { Cell } from "./Cell.js";
+
 export const Board = (props) => {
-	return <div className="board"></div>;
+	const cells = props.cells.map((cell, index) => {
+		return (
+			<Cell
+				key={index}
+				className={cell.isSelected ? "cell selected" : "cell"}
+				index={index}
+				tabIndex={cell.tabIndex}
+				number={cell.number}
+				letter={cell.letter}
+				isSelected={cell.isSelected}
+				isBlackSquare={cell.isBlackSquare}
+			/>
+		);
+	});
+
+	return <div className="board">{cells}</div>;
 };
