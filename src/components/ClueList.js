742a389b7af6d900688435eeacc340f4d954b3cf
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-02 23:27:19 -0500

    Refactor ClueList to dynamically render list items

--- src/components/ClueList.js
+++ src/components/ClueList.js
@@ -1,8 +1,28 @@
-export const ClueList = (props) => {
+/* export const ClueList = (props) => {
 	return (
 		<div className="clue-list-wrapper">
 			<h3 className="clue-list-title">{props.direction}</h3>
 			<ol className="clue-list">{props.children}</ol>
+		</div>
+	);
+}; */
+
+import { ClueListItem } from "./ClueListItem.js";
+
+export const ClueList = (props) => {
+	return (
+		<div className="clue-list-wrapper">
+			<h3 className="clue-list-title">{props.direction}</h3>
+			<ol className="clue-list">
+				{props.clueNumbers.map((number) => {
+					return (
+						<ClueListItem
+							key={`${number}-${props.direction}`}
+							clueLabel={number}
+						/>
+					);
+				})}
+			</ol>
 		</div>
 	);
 };
