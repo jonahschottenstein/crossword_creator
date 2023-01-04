e7f03088051b9f8940aaa3c33dcc038c62c25788
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-03 21:59:56 -0500

    Update className with "isInSelectedWord" condition

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,13 +1,31 @@
 import { Cell } from "./Cell.js";
+import {
+	getWords,
+	getSelectedWordObject,
+	isInSelectedWord,
+} from "../utilities/words.js";
 
 export const Board = (props) => {
+	const wordsObject = getWords(props.direction, props.cells);
+	console.log(wordsObject);
+	const selectedWord = getSelectedWordObject(wordsObject).word;
+
 	const cells = props.cells.map((cell, index) => {
 		return (
 			<Cell
 				key={index}
-				className={
+				/* className={
 					cell.isSelected
 						? "cell selected"
+						: cell.isBlackSquare
+						? "cell cell-block"
+						: "cell"
+				} */
+				className={
+					cell.isSelected
+						? "cell selected highlighted"
+						: isInSelectedWord(selectedWord, cell)
+						? "cell highlighted"
 						: cell.isBlackSquare
 						? "cell cell-block"
 						: "cell"
