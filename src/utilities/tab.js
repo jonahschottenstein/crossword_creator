6adbefd442d12bded89e47ad35aa8b4230ca557a
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-05 01:32:26 -0500

    Add methods for shift+tab keyboard navigation
    Not currently working. Think direction is changing too late.

--- src/utilities/tab.js
+++ src/utilities/tab.js
@@ -5,6 +5,70 @@ import {
 	getFirstBlankInWord,
 } from "./words.js";
 
+export const handleShiftTabKeyDirectionChange = (
+	e,
+	direction,
+	setDirection,
+	cells
+) => {
+	const firstAvailableWordObject = getFirstOrLastAvailableWordObject(
+		"first",
+		direction,
+		cells
+	);
+	const firstAvailableWordIsSelected = wordIsSelected(
+		firstAvailableWordObject.index,
+		direction,
+		cells
+	);
+
+	if (!e.shiftKey || e.key !== "Tab") return;
+
+	if (firstAvailableWordIsSelected) {
+		setDirection((d) => (d === "across" ? "down" : "across"));
+	}
+};
+
+export const handleShiftTabKeyMovement = (
+	e,
+	direction,
+	setDirection,
+	cells
+) => {
+	if (!e.shiftKey || e.key !== "Tab") return;
+	e.preventDefault();
+
+	const firstBlankInPreviousAvailableWord = getFirstBlankInWord(
+		getClosestAvailableWord("before", direction, cells)
+	);
+	const firstBlankInLastAvailableWord = getFirstBlankInWord(
+		getFirstOrLastAvailableWordObject("last", direction, cells).word
+	);
+	console.log("1stBlankLastWord", firstBlankInLastAvailableWord);
+	const firstBlankElement =
+		document.getElementsByClassName("cell")[
+			firstBlankInLastAvailableWord.index
+		];
+	const firstAvailableWordIsSelected = wordIsSelected(
+		getFirstOrLastAvailableWordObject("first", direction, cells).index,
+		direction,
+		cells
+	);
+
+	if (firstAvailableWordIsSelected) {
+		handleShiftTabKeyDirectionChange(e, direction, setDirection, cells);
+		firstBlankElement.click();
+		firstBlankElement.focus({ preventScroll: true });
+	} else {
+		const firstBlankElementInPreviousAvailableWord =
+			document.getElementsByClassName("cell")[
+				firstBlankInPreviousAvailableWord.index
+			];
+		firstBlankElementInPreviousAvailableWord.click();
+		firstBlankElementInPreviousAvailableWord.focus({ preventScroll: true });
+	}
+};
+
 export const handleTabKeyDirectionChange = (
 	e,
 	direction,
