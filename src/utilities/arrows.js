d02831a5bdc9441af1d657ac9b247ae64155d87a
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-04 23:30:49 -0500

    Add methods to navigate the board using arrow keys

--- src/utilities/arrows.js
+++ src/utilities/arrows.js
@@ -9,3 +9,55 @@ export const handleArrowKeyDirectionChange = (e, direction, setDirection) => {
 		}
 	}
 };
+
+const cell1IsBeforeCell2 = (cell1, cell2) => cell1.index < cell2.index;
+
+const getNextAvailableCell = (e, cellRowOrColumn, selectedCell) => {
+	const arrayMethod =
+		e.key === "ArrowLeft" || e.key === "ArrowUp" ? "findLast" : "find";
+
+	const nextAvailableCell = cellRowOrColumn[arrayMethod]((cell) => {
+		const isBeforeOrAfter =
+			e.key === "ArrowLeft" || e.key === "ArrowUp"
+				? cell1IsBeforeCell2(cell, selectedCell)
+				: cell1IsBeforeCell2(selectedCell, cell);
+		return isBeforeOrAfter && !cell.isBlackSquare;
+	});
+	return nextAvailableCell;
+};
+
+const getLastAvailableCell = (e, cellRowOrColumn) => {
+	const arrayMethod =
+		e.key === "ArrowLeft" || e.key === "ArrowUp" ? "find" : "findLast";
+	const lastAvailableCell = cellRowOrColumn[arrayMethod](
+		(cell) => !cell.isBlackSquare
+	);
+	return lastAvailableCell;
+};
+
+export const handleArrowKeyMovement = (e, direction, cells) => {
+	if (direction === "across") {
+		if (e.key !== "ArrowLeft" && e.key !== "ArrowRight") return;
+	} else {
+		if (e.key !== "ArrowUp" && e.key !== "ArrowDown") return;
+	}
+
+	const selectedCell = cells.find((cell) => cell.isSelected);
+	const rowOrColumn = direction === "across" ? "row" : "column";
+	const cellRowOrColumn = cells.filter(
+		(cell) => cell[rowOrColumn] === selectedCell[rowOrColumn]
+	);
+	const nextAvailableCell = getNextAvailableCell(
+		e,
+		cellRowOrColumn,
+		selectedCell
+	);
+	const lastAvailableCell = getLastAvailableCell(e, cellRowOrColumn);
+
+	if (lastAvailableCell.index === selectedCell.index) return;
+
+	const nextAvailableCellElement =
+		document.getElementsByClassName("cell")[nextAvailableCell.index];
+	nextAvailableCellElement.click();
+	nextAvailableCellElement.focus({ preventScroll: true });
+};
