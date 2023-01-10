0e788821c4c0cf4c696778fb9c71a0561b0ec92e
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-10 16:33:36 -0500

    Delete old code

--- src/utilities/tab.js
+++ src/utilities/tab.js
@@ -1,28 +1,10 @@
 import {
-	getFirstAvailableWord,
-	getLastAvailableWord,
-	getPreviousAvailableWord,
-	getNextAvailableWord,
-	getRemainingAvailableWords,
 	getRemainingOpenWords,
 	getLastOpenWord,
 	getFirstOpenWord,
 	getPreviousOpenWord,
 	getNextOpenWord,
 } from "./words.js";
-
-/* export const handleTabDirectionChange = (e, direction, setDirection, cells) => {
-	const isShiftKeyPressed = e.shiftKey;
-	if (e.key !== "Tab") return;
-	e.preventDefault();
-
-	const remainingAvailableWords = getRemainingAvailableWords(direction, cells)[
-		isShiftKeyPressed ? "before" : "after"
-	];
-
-	if (remainingAvailableWords.length !== 0) return;
-	setDirection((d) => (d === "across" ? "down" : "across"));
-}; */
 
 export const handleTabDirectionChange = (e, direction, setDirection, cells) => {
 	const isShiftKeyPressed = e.shiftKey;
@@ -41,35 +23,6 @@ const getCellElement = (cell) => {
 	const cellElements = document.getElementsByClassName("cell");
 	return cellElements[cell.index];
 };
-
-/* export const handleTabMovement = (e, direction, cells) => {
-	const isShiftKeyPressed = e.shiftKey;
-	if (e.key !== "Tab") return;
-	e.preventDefault();
-
-	const nextDirection = direction === "across" ? "down" : "across";
-	const firstAvailableWordNextDirection = isShiftKeyPressed
-		? getLastAvailableWord(nextDirection, cells)
-		: getFirstAvailableWord(nextDirection, cells);
-	const nextAvailableWord = isShiftKeyPressed
-		? getPreviousAvailableWord(direction, cells)
-		: getNextAvailableWord(direction, cells);
-	const remainingAvailableWords = getRemainingAvailableWords(direction, cells)[
-		isShiftKeyPressed ? "before" : "after"
-	];
-
-	if (remainingAvailableWords.length === 0) {
-		getCellElement(firstAvailableWordNextDirection.firstBlankCell).click();
-		getCellElement(firstAvailableWordNextDirection.firstBlankCell).focus({
-			preventScroll: true,
-		});
-	} else {
-		getCellElement(nextAvailableWord.firstBlankCell).click();
-		getCellElement(nextAvailableWord.firstBlankCell).focus({
-			preventScroll: true,
-		});
-	}
-}; */
 
 export const handleTabMovement = (e, direction, cells) => {
 	const isShiftKeyPressed = e.shiftKey;
