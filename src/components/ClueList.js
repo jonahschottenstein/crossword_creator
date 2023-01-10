215cf0ac24053988a6af5fbbb30d6ee5a07b6ea2
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-10 18:02:44 -0500

    Delete old code

--- src/components/ClueList.js
+++ src/components/ClueList.js
@@ -1,12 +1,3 @@
-/* export const ClueList = (props) => {
-	return (
-		<div className="clue-list-wrapper">
-			<h3 className="clue-list-title">{props.direction}</h3>
-			<ol className="clue-list">{props.children}</ol>
-		</div>
-	);
-}; */
-
 import { ClueListItem } from "./ClueListItem.js";
 
 export const ClueList = (props) => {
