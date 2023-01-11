73688eb200bc386bcb0e316b3f0e19ed6a07b9dc
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-11 07:34:22 -0500

    Add and refactor setCellLetter and extract methods

---
+++ src/utilities/setCellLetter.js
@@ -0,0 +1,20 @@
+const entryIsValid = (e) =>
+	!e.metaKey && !e.altKey && !e.ctrlKey && /\b[A-Za-z0-9]{1}\b/.test(e.key);
+
+const getSelectedCell = (cell) => cell.isSelected;
+
+export const setCellLetter = (e, cells, setCells) => {
+	if (!entryIsValid(e)) return;
+	const selectedCellIndex = cells.findIndex(getSelectedCell);
+
+	setCells((prevState) => {
+		const newState = prevState.map((cell) => {
+			if (cell.index === selectedCellIndex) {
+				return { ...cell, letter: e.key.toUpperCase() };
+			} else {
+				return cell;
+			}
+		});
+		return newState;
+	});
+};
