7d2f707fbe70d684db28863a7fce211796349bf6
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-10 22:55:08 -0500

    Add setCellNumbers from App.js

---
+++ src/utilities/setCellNumbers.js
@@ -0,0 +1,13 @@
+export const setCellNumbers = (setCells) => {
+	setCells((prevState) => {
+		const numberedCells = getNumberedCells(prevState);
+		const newState = prevState.map((cell) => {
+			if (numberedCells.includes(cell)) {
+				return { ...cell, number: numberedCells.indexOf(cell) + 1 };
+			} else {
+				return { ...cell, number: null };
+			}
+		});
+		return newState;
+	});
+};
