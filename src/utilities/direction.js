f0ffd40dad368584fb3309ec31fa2e1510fc67a8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-10 21:35:12 -0500

    Add and rename setPuzzleDirection
    Add to shorten length of App.js, and rename to 'setDirectionOnClick' to
    be more descriptive

--- src/utilities/direction.js
+++ src/utilities/direction.js
@@ -1,1 +1,8 @@
+export const setDirectionOnClick = (e, cells, setDirection) => {
+	const selectedCellIndex = cells.findIndex((cell) => cell.isSelected);
+	const targetIndex = Number(e.target.dataset.index);
+	const selectedCellIsClicked = selectedCellIndex === targetIndex;
 
+	if (!selectedCellIsClicked) return;
+	setDirection((d) => (d === "across" ? "down" : "across"));
+};
