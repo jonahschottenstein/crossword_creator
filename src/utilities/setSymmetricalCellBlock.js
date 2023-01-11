10bd1daf0172d495a02755b39537d8cb8a4a4210
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-10 22:45:04 -0500

    Add setSymmetricalCellBlock from App.js

---
+++ src/utilities/setSymmetricalCellBlock.js
@@ -0,0 +1,21 @@
+export const setSymmetricalCellBlock = (e, setCells) => {
+	/* Not sure if when targetCell is (not) a cellBlock and symmetricalCell is the opposite if it should toggle both or set them to be the same first. */
+	const numberOfCells = cells.length;
+	const lastCellIndex = numberOfCells - 1;
+	const centerCellIndex = Math.floor(numberOfCells / 2);
+	const targetIndex = Number(e.target.dataset.index);
+	const symmetricalCellIndex = lastCellIndex - targetIndex;
+
+	if (targetIndex !== centerCellIndex) {
+		setCells((prevState) => {
+			const newState = prevState.map((cell) => {
+				if (cell.index === symmetricalCellIndex) {
+					return { ...cell, isBlackSquare: !cell.isBlackSquare };
+				} else {
+					return cell;
+				}
+			});
+			return newState;
+		});
+	}
+};
