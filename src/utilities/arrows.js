530825a2baa7159794e8c2dc10bf147c55752af0
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-19 15:48:30 -0500

    Update methods to set direction and select element

--- src/utilities/arrows.js
+++ src/utilities/arrows.js
@@ -1,4 +1,8 @@
-export const handleArrowKeyDirectionChange = (e, direction, setDirection) => {
+// import { getSelectedCell, selectCellElement } from "./letters";
+// import { getCellElement } from "./tab";
+import { getCellBefore, getCellAfter, selectCellElement } from "./helpers";
+
+/* export const handleArrowKeyDirectionChange = (e, direction, setDirection) => {
 	e.preventDefault();
 
 	if (direction === "down") {
@@ -64,4 +68,150 @@ export const handleArrowKeyMovement = (e, direction, cells) => {
 		document.getElementsByClassName("cell")[nextAvailableCell.index];
 	nextAvailableCellElement.click();
 	nextAvailableCellElement.focus({ preventScroll: true });
+}; */
+
+/* ************************************************************************ */
+
+// export const cell1IsBeforeCell2 = (cell1, cell2) => cell1.index < cell2.index;
+
+const changeDirectionOnArrowKey = (e, direction, setDirection) => {
+	e.preventDefault();
+
+	if (direction === "down") {
+		if (e.key === "ArrowLeft" || e.key === "ArrowRight") {
+			setDirection("across");
+		}
+	} else {
+		if (e.key === "ArrowUp" || e.key === "ArrowDown") {
+			setDirection("down");
+		}
+	}
+};
+
+const getSelectedRow = (cells) => {
+	const selectedCell = cells.find((cell) => cell.isSelected);
+	return cells.filter((cell) => cell.row === selectedCell.row);
+};
+
+const getSelectedColumn = (cells) => {
+	const selectedCell = cells.find((cell) => cell.isSelected);
+	return cells.filter((cell) => cell.column === selectedCell.column);
+};
+
+const getGridSectionCells = (direction, cells) => {
+	const selectedRow = getSelectedRow(cells);
+	const selectedColumn = getSelectedColumn(cells);
+	const gridSection = direction === "across" ? selectedRow : selectedColumn;
+	const gridSectionCells = gridSection.filter((cell) => !cell.isBlackSquare);
+
+	return gridSectionCells;
+};
+
+/* export const getCellBefore = (selectedCell, cellsArray) =>
+	cellsArray.findLast((cell) => cell1IsBeforeCell2(cell, selectedCell));
+
+export const getCellAfter = (selectedCell, cellsArray) =>
+	cellsArray.find((cell) => cell1IsBeforeCell2(selectedCell, cell)); */
+
+/* const getNextCellOnArrow = (e, direction, cells) => {
+	const selectedCell = getSelectedCell(direction, cells);
+	const gridSectionCells = getGridSectionCells(direction, cells);
+	const cellBefore = getCellBefore(selectedCell, gridSectionCells);
+	const cellAfter = getCellAfter(selectedCell, gridSectionCells);
+
+	if (direction === "across") {
+		if (e.key === "ArrowLeft") return cellBefore;
+		if (e.key === "ArrowRight") return cellAfter;
+	} else {
+		if (e.key === "ArrowUp") return cellBefore;
+		if (e.key === "ArrowDown") return cellAfter;
+	}
+}; */
+/* const getCellToSelectOnArrowKey = (e, direction, cells) => {
+	const selectedCell = getSelectedCell(direction, cells);
+	const gridSectionCells = getGridSectionCells(direction, cells);
+	const cellBefore = getCellBefore(selectedCell, gridSectionCells);
+	const cellAfter = getCellAfter(selectedCell, gridSectionCells);
+
+	if (direction === "across") {
+		if (e.key === "ArrowLeft") return cellBefore;
+		if (e.key === "ArrowRight") return cellAfter;
+	} else {
+		if (e.key === "ArrowUp") return cellBefore;
+		if (e.key === "ArrowDown") return cellAfter;
+	}
+}; */
+
+/* const getNextCellOnArrowKey = (e, direction, cells) => {
+	const selectedCell = getSelectedCell(direction, cells);
+	const gridSectionCells = getGridSectionCells(direction, cells);
+	const cellBefore = getCellBefore(selectedCell, gridSectionCells);
+	const cellAfter = getCellAfter(selectedCell, gridSectionCells);
+
+	if (direction === "across") {
+		if (e.key === "ArrowLeft") return cellBefore;
+		if (e.key === "ArrowRight") return cellAfter;
+	} else {
+		if (e.key === "ArrowUp") return cellBefore;
+		if (e.key === "ArrowDown") return cellAfter;
+	}
+}; */
+const getNextCellOnArrowKey = (e, direction, cells) => {
+	const gridSectionCells = getGridSectionCells(direction, cells);
+	const cellBefore = getCellBefore(gridSectionCells);
+	const cellAfter = getCellAfter(gridSectionCells);
+
+	if (direction === "across") {
+		if (e.key === "ArrowLeft") return cellBefore;
+		if (e.key === "ArrowRight") return cellAfter;
+	} else {
+		if (e.key === "ArrowUp") return cellBefore;
+		if (e.key === "ArrowDown") return cellAfter;
+	}
+};
+
+const arrowIsPressed = (e) => {
+	return (
+		e.key === "ArrowLeft" ||
+		e.key === "ArrowRight" ||
+		e.key === "ArrowUp" ||
+		e.key === "ArrowDown"
+	);
+};
+
+/* const selectNextCellElementOnArrow = (e, direction, cells) => {
+	if (!arrowIsPressed(e)) return;
+
+	const nextCell = getNextCellOnArrow(e, direction, cells);
+
+	if (!nextCell) return;
+
+	selectCellElement(nextCell);
+}; */
+/* const selectCellElementOnArrowKey = (e, direction, cells) => {
+	if (!arrowIsPressed(e)) return;
+
+	const cellToSelect = getCellToSelectOnArrowKey(e, direction, cells);
+
+	if (!cellToSelect) return;
+
+	selectCellElement(cellToSelect);
+}; */
+const selectCellElementOnArrowKey = (e, direction, cells) => {
+	if (!arrowIsPressed(e)) return;
+
+	const nextCell = getNextCellOnArrowKey(e, direction, cells);
+
+	if (!nextCell) return;
+
+	selectCellElement(nextCell);
+};
+
+/* export const handleArrowKeys = (e, direction, setDirection, cells) => {
+	changeDirectionOnArrowKey(e, direction, setDirection);
+	selectNextCellElementOnArrow(e, direction, cells);
+}; */
+export const handleArrowKeys = (e, direction, setDirection, cells) => {
+	changeDirectionOnArrowKey(e, direction, setDirection);
+	selectCellElementOnArrowKey(e, direction, cells);
 };
