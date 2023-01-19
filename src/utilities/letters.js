bce1755095aa6ac12291d1f827317ab9a5b1df2d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-19 16:24:52 -0500

    Update methods to get next cell

--- src/utilities/letters.js
+++ src/utilities/letters.js
@@ -1,34 +1,61 @@
-import {
-	cellHasLetter,
+/* import {
+	// cellHasLetter,
 	getFirstOpenWord,
 	getNextOpenWord,
 	getSelectedWord,
 	getRemainingOpenWords,
-} from "./words.js";
-import { getCellElement } from "./tab.js";
+	getWordObj,
+} from "./words.js"; */
+// import { getSelectedWord, getWordObj } from "./words.js";
+import { getWordObj } from "./words.js";
+// import { getCellElement } from "./tab.js";
+// import { entryIsValid, setCellLetter } from "./setCellLetter.js";
+import { setCellLetter } from "./setCellLetter.js";
+// import { cell1IsBeforeCell2, getCellAfter } from "./arrows.js";
+// import { cell1IsBeforeCell2 } from "./arrows.js";
+import {
+	cellHasLetter,
+	entryIsValid,
+	changeDirection,
+	getNextDirection,
+	getSelectedCell,
+	getBlankBefore,
+	getBlankAfter,
+	getCellAfter,
+	selectCellElement,
+} from "./helpers.js";
 
-export const getNextDirection = (direction) =>
-	direction === "across" ? "down" : "across";
+/* export const getNextDirection = (direction) =>
+	direction === "across" ? "down" : "across"; */
 
-export const getSelectedCell = (direction, cells) => {
+/* export const getSelectedCell = (direction, cells) => {
 	const selectedWord = getSelectedWord(direction, cells).word;
 	const selectedCell = selectedWord.find((cell) => cell.isSelected);
 
 	return selectedCell;
-};
+}; */
 
-export const getLastCellOfWord = (word) => word.findLast((cell) => cell);
+/* export const getBlankBefore = (selectedCell, cellsArray) =>
+	cellsArray.findLast(
+		(cell) => cell1IsBeforeCell2(cell, selectedCell) && !cell.letter
+	); */
+/* export const getBlankAfter = (selectedCell, cellsArray) =>
+	cellsArray.find(
+		(cell) => cell1IsBeforeCell2(selectedCell, cell) && !cell.letter
+	); */
 
-const getNextCellInSelectedWord = (direction, cells) => {
+// export const getLastCellOfWord = (word) => word.findLast((cell) => cell);
+
+/* const getNextCellInSelectedWord = (direction, cells) => {
 	const selectedWord = getSelectedWord(direction, cells).word;
 	const selectedCell = getSelectedCell(direction, cells);
 	const lastCellOfWord = getLastCellOfWord(selectedWord);
 	const nextCell = selectedWord.find((cell) => cell.index > selectedCell.index);
 
 	return !lastCellOfWord.isSelected && nextCell;
-};
+}; */
 
-const getRemainingBlanksInWord = (direction, cells) => {
+/* const getRemainingBlanksInWord = (direction, cells) => {
 	const selectedWord = getSelectedWord(direction, cells).word;
 	const selectedCell = getSelectedCell(direction, cells);
 
@@ -43,38 +70,38 @@ const getRemainingBlanksInWord = (direction, cells) => {
 			return this.before.concat(this.after);
 		},
 	};
-};
+}; */
 
-const getNextBlankInWord = (direction, cells) => {
+/* const getNextBlankInWord = (direction, cells) => {
 	const wordBlanksAfter = getRemainingBlanksInWord(direction, cells).after;
 	const nextBlankInWord = !!wordBlanksAfter.length && wordBlanksAfter[0];
 
 	return nextBlankInWord;
-};
+}; */
 
-const getFirstBlankBeforeInWord = (direction, cells) => {
+/* const getFirstBlankBeforeInWord = (direction, cells) => {
 	const wordBlanksBefore = getRemainingBlanksInWord(direction, cells).before;
 	const firstBlankBefore = !!wordBlanksBefore.length && wordBlanksBefore[0];
 
 	return firstBlankBefore;
-};
+}; */
 
-const getFirstBlankInNextOpenWord = (direction, cells) => {
+/* const getFirstBlankInNextOpenWord = (direction, cells) => {
 	const nextOpenWord = getNextOpenWord(direction, cells);
 	const firstBlankInNextOpenWord = nextOpenWord && nextOpenWord.firstBlankCell;
 
 	return firstBlankInNextOpenWord;
-};
+}; */
 
-const getFirstBlankNextDirection = (direction, cells) => {
+/* const getFirstBlankNextDirection = (direction, cells) => {
 	const nextDirection = getNextDirection(direction);
 	const firstOpenWordNextDirection = getFirstOpenWord(nextDirection, cells);
 	const firstBlankNextDirection = firstOpenWordNextDirection.firstBlankCell;
 
 	return firstBlankNextDirection;
-};
+}; */
 
-const getNextCellToSelect = (direction, cells) => {
+/* const getNextCellToSelect = (direction, cells) => {
 	const selectedCell = getSelectedCell(direction, cells);
 	const selectedCellHasLetter = cellHasLetter(selectedCell);
 	const nextCellInWord = getNextCellInSelectedWord(direction, cells);
@@ -91,6 +118,113 @@ const getNextCellToSelect = (direction, cells) => {
 		firstOption ||
 		firstBlankBeforeInWord ||
 		firstBlankInNextOpenWord ||
+		firstBlankNextDirection
+	);
+}; */
+/* const getCellToSelectOnLetterKey = (direction, cells) => {
+	const selectedCell = getSelectedCell(direction, cells);
+	const selectedCellHasLetter = cellHasLetter(selectedCell);
+	const nextCellInWord = getNextCellInSelectedWord(direction, cells);
+	const nextBlankInWord = getNextBlankInWord(direction, cells);
+	const firstBlankBeforeInWord = getFirstBlankBeforeInWord(direction, cells);
+	const firstBlankInNextOpenWord = getFirstBlankInNextOpenWord(
+		direction,
+		cells
+	);
+	const firstBlankNextDirection = getFirstBlankNextDirection(direction, cells);
+	const firstOption = selectedCellHasLetter ? nextCellInWord : nextBlankInWord;
+
+	return (
+		firstOption ||
+		firstBlankBeforeInWord ||
+		firstBlankInNextOpenWord ||
+		firstBlankNextDirection
+	);
+}; */
+
+/* const getCellToSelectOnLetterKey = (direction, cells) => {
+	const selectedCell = getSelectedCell(direction, cells);
+	const selectedCellHasLetter = cellHasLetter(selectedCell);
+	const nextDirection = getNextDirection(direction);
+	const { selectedWordObj, openWordObjAfter } = getWordObj(direction, cells);
+	const { firstOpenWordObj: firstOpenWordObjNextDirection } = getWordObj(
+		nextDirection,
+		cells
+	);
+	const wordCellAfter = getCellAfter(selectedCell, selectedWordObj.word);
+	const wordBlankBefore = getBlankBefore(selectedCell, selectedWordObj.word);
+	const wordBlankAfter = getBlankAfter(selectedCell, selectedWordObj.word);
+	const firstBlankBeforeInWord = wordBlankBefore && selectedWordObj.firstBlank;
+	const firstBlankInOpenWordAfter = openWordObjAfter?.firstBlank;
+	const firstBlankNextDirection = firstOpenWordObjNextDirection.firstBlank;
+	const firstOption = selectedCellHasLetter ? wordCellAfter : wordBlankAfter;
+
+	return (
+		firstOption ||
+		firstBlankBeforeInWord ||
+		firstBlankInOpenWordAfter ||
+		firstBlankNextDirection
+	);
+}; */
+/* const getCellToSelectOnLetterKey = (direction, cells) => {
+	const selectedCell = getSelectedCell(direction, cells);
+	const selectedCellHasLetter = cellHasLetter(selectedCell);
+	const nextDirection = getNextDirection(direction);
+	const { selectedWordObj, openWordObjAfter } = getWordObj(direction, cells);
+	const { firstOpenWordObj: firstOpenWordObjNextDirection } = getWordObj(
+		nextDirection,
+		cells
+	);
+	const wordCellAfter = getCellAfter(selectedWordObj.word);
+	const wordBlankBefore = getBlankBefore(selectedCell, selectedWordObj.word);
+	const wordBlankAfter = getBlankAfter(selectedCell, selectedWordObj.word);
+	const firstBlankBeforeInWord = wordBlankBefore && selectedWordObj.firstBlank;
+	const firstBlankInOpenWordAfter = openWordObjAfter?.firstBlank;
+	const firstBlankNextDirection = firstOpenWordObjNextDirection.firstBlank;
+	const firstOption = selectedCellHasLetter ? wordCellAfter : wordBlankAfter;
+
+	return (
+		firstOption ||
+		firstBlankBeforeInWord ||
+		firstBlankInOpenWordAfter ||
+		firstBlankNextDirection
+	);
+}; */
+
+const getFirstBlank = (direction, cells) => {
+	const { firstOpenWordObj } = getWordObj(direction, cells);
+	const { firstBlank } = firstOpenWordObj;
+
+	return firstBlank;
+};
+
+const getNextCellFirstOptionOnLetterKey = (direction, cells) => {
+	const selectedCell = getSelectedCell(cells);
+	const selectedCellHasLetter = cellHasLetter(selectedCell);
+	const { selectedWordObj } = getWordObj(direction, cells);
+	const cellAfterInWord = getCellAfter(selectedWordObj.word);
+	const blankAfterInWord = getBlankAfter(selectedWordObj.word);
+	const firstOption = selectedCellHasLetter
+		? cellAfterInWord
+		: blankAfterInWord;
+
+	return firstOption;
+};
+
+const getNextCellOnLetterKey = (direction, cells) => {
+	const nextDirection = getNextDirection(direction);
+	const { selectedWordObj, openWordObjAfter } = getWordObj(direction, cells);
+	const firstOption = getNextCellFirstOptionOnLetterKey(direction, cells);
+	const blankBeforeInWord = getBlankBefore(selectedWordObj.word);
+	const firstBlankBeforeInWord =
+		blankBeforeInWord && selectedWordObj.firstBlank;
+	const firstBlankInOpenWordAfter = openWordObjAfter?.firstBlank;
+	const firstBlankNextDirection = getFirstBlank(nextDirection, cells);
+
+	return (
+		firstOption ||
+		firstBlankBeforeInWord ||
+		firstBlankInOpenWordAfter ||
 		firstBlankNextDirection
 	);
 };
