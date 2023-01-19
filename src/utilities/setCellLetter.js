1589a413a3b210393f649ed5bc9be5444b7158b8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-19 16:35:18 -0500

    Update setCellLetter

--- src/utilities/setCellLetter.js
+++ src/utilities/setCellLetter.js
@@ -1,15 +1,48 @@
-export const entryIsValid = (e) =>
-	!e.metaKey && !e.altKey && !e.ctrlKey && /\b[A-Za-z0-9]{1}\b/.test(e.key);
+import { getSelectedCell, entryIsValid } from "./helpers";
 
-const getSelectedCell = (cell) => cell.isSelected;
+// export const entryIsValid = (e) =>
+// 	!e.metaKey && !e.altKey && !e.ctrlKey && /\b[A-Za-z0-9]{1}\b/.test(e.key);
 
-export const setCellLetter = (e, cells, setCells) => {
+// const getSelectedCell = (cell) => cell.isSelected;
+
+/* export const setCellLetter = (e, cells, setCells) => {
 	if (!entryIsValid(e)) return;
 	const selectedCellIndex = cells.findIndex(getSelectedCell);
 
 	setCells((prevState) => {
 		const newState = prevState.map((cell) => {
 			if (cell.index === selectedCellIndex) {
+				return { ...cell, letter: e.key.toUpperCase() };
+			} else {
+				return cell;
+			}
+		});
+		return newState;
+	});
+}; */
+/* export const setCellLetter = (e, cells, setCells) => {
+	if (!entryIsValid(e)) return;
+
+	const selectedCell = cells.find((cell) => cell.isSelected);
+	// const selectedCellIndex = cells.findIndex(getSelectedCell);
+
+	setCells((prevState) => {
+		const newState = prevState.map((cell) => {
+			if (cell.index === selectedCell.index) {
+				return { ...cell, letter: e.key.toUpperCase() };
+			} else {
+				return cell;
+			}
+		});
+		return newState;
+	});
+}; */
+export const setCellLetter = (e, cells, setCells) => {
+	const selectedCell = getSelectedCell(cells);
+
+	setCells((prevState) => {
+		const newState = prevState.map((cell) => {
+			if (cell.index === selectedCell.index) {
 				return { ...cell, letter: e.key.toUpperCase() };
 			} else {
 				return cell;
