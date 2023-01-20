73caaf6f7cd405b7da605acfc7e2322edc01d350
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-20 16:50:04 -0500

    Add methods to select word on Board from li click

--- src/utilities/clueListItems.js
+++ src/utilities/clueListItems.js
@@ -1,4 +1,5 @@
-import { getWordObj } from "./words";
+import { changeDirection, selectCellElement } from "./helpers";
+import { getWordObj, getWordObjs } from "./words";
 
 export const getClueProps = (direction, cells) => {
 	const { selectedWordObj } = getWordObj(direction, cells);
@@ -39,4 +40,58 @@ export const scrollToLi = (direction, cells) => {
 		top: clueLi.offsetTop - 47,
 		behavior: "smooth",
 	});
+};
+
+export const getClueLiNumber = (e) => {
+	if (!e.target.matches(".clue-list-item")) return;
+
+	const clueLiNumber = Number(
+		e.target.querySelector(".clue-label").textContent
+	);
+
+	return clueLiNumber;
+};
+
+export const getClueLiDirection = (e) => {
+	if (!e.target.matches(".clue-list-item")) return;
+
+	const closestWrapper = e.target.closest(".clue-list-wrapper");
+	const clueLiDirection =
+		closestWrapper.querySelector(".clue-list-title").textContent;
+
+	return clueLiDirection;
+};
+
+export const getWordToHighlight = (e, cells) => {
+	const clueLiNumber = getClueLiNumber(e);
+	const clueLiDirection = getClueLiDirection(e);
+	const wordObjs = getWordObjs(clueLiDirection, cells);
+
+	const wordToHighlight = wordObjs.find(
+		(obj) => obj.clueNumber === clueLiNumber
+	);
+
+	return wordToHighlight;
+};
+
+export const getCellOnLiClick = (e, cells) => {
+	const wordToHighlight = getWordToHighlight(e, cells);
+
+	return wordToHighlight.firstBlank || wordToHighlight.firstCell;
+};
+
+export const selectCellElementOnLiClick = (
+	e,
+	direction,
+	setDirection,
+	cells
+) => {
+	const cellToSelect = getCellOnLiClick(e, cells);
+	const clueLiDirection = getClueLiDirection(e);
+
+	if (direction !== clueLiDirection) {
+		changeDirection(setDirection);
+	}
+
+	selectCellElement(cellToSelect);
 };
