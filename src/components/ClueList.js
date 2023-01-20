0fb3d7388dea192e33792a6a6266474cfafb71ba
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-20 16:06:23 -0500

    Add id and update className for highlighting

--- src/components/ClueList.js
+++ src/components/ClueList.js
@@ -4,12 +4,21 @@ export const ClueList = (props) => {
 	return (
 		<div className="clue-list-wrapper">
 			<h3 className="clue-list-title">{props.direction}</h3>
-			<ol className="clue-list">
+			<ol id={`${props.direction}-clue-list`} className="clue-list">
 				{props.clueNumbers.map((number) => {
 					return (
 						<ClueListItem
 							key={`${number}-${props.direction}`}
 							clueLabel={number}
+							className={
+								props.clueProps.clueNumber === number &&
+								props.clueProps.clueDirection === props.direction
+									? "clue-list-item highlighted"
+									: props.oppositeClueProps.clueNumber === number &&
+									  props.oppositeClueProps.clueDirection === props.direction
+									? "clue-list-item opposite-highlighted"
+									: "clue-list-item"
+							}
 						/>
 					);
 				})}
