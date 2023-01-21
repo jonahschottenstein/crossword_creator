9fbb5d1e482962c7ab6256f373722ced66e3da0f
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-21 12:38:46 -0500

    Add button components to toggle page visibility

--- src/components/DashboardHeader.js
+++ src/components/DashboardHeader.js
@@ -1,3 +1,23 @@
+import { DashboardHeaderButton } from "./DashboardHeaderButton";
+
 export const DashboardHeader = (props) => {
-	return <div className="dashboard-header"></div>;
+	return (
+		<div className="dashboard-header">
+			<DashboardHeaderButton
+				buttonLabel={"Stats"}
+				visibleDashPage={props.visibleDashPage}
+				onChange={props.onChange}
+			/>
+			<DashboardHeaderButton
+				buttonLabel={"Clues"}
+				visibleDashPage={props.visibleDashPage}
+				onChange={props.onChange}
+			/>
+			<DashboardHeaderButton
+				buttonLabel={"Fill"}
+				visibleDashPage={props.visibleDashPage}
+				onChange={props.onChange}
+			/>
+		</div>
+	);
 };
