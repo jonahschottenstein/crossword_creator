eaa8aa85f47c307b98bfb38dad11eb326b84f943
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-21 12:32:19 -0500

    Use props to toggle visible dashboard page

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,3 +1,43 @@
+import { ClueListsContainer } from "./ClueListsContainer";
+import { DashboardStatsTable } from "./DashboardStatsTable";
+import { getStats } from "../utilities/stats";
+
 export const DashboardPage = (props) => {
-	return <div className="dashboard-page"></div>;
+	const {
+		gridSize,
+		totalWordCount,
+		blackSquareCount,
+		avgWordLength,
+		scrabbleScore,
+		pangram,
+	} = getStats(props.direction, props.cells);
+
+	if (props.visibleDashPage === "Stats") {
+		return (
+			<div className="dashboard-page">
+				<DashboardStatsTable
+					gridSize={gridSize}
+					totalWordCount={totalWordCount}
+					blackSquareCount={blackSquareCount}
+					avgWordLength={avgWordLength}
+					scrabbleScore={scrabbleScore}
+					pangram={pangram}
+				/>
+			</div>
+		);
+	} else if (props.visibleDashPage === "Clues") {
+		return (
+			<div className="dashboard-page">
+				<ClueListsContainer
+					acrossClueNumbers={props.acrossClueNumbers}
+					downClueNumbers={props.downClueNumbers}
+					clueProps={props.clueProps}
+					oppositeClueProps={props.oppositeClueProps}
+					onClick={props.onClick}
+				/>
+			</div>
+		);
+	} else {
+		return <div className="dashboard-page">{props.pageTitle}</div>;
+	}
 };
