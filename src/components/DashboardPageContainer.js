90c3dbac22c32a066a83a3499ab83c3e6c20daac
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-21 12:33:38 -0500

    Pass props to DashboardPage component

--- src/components/DashboardPageContainer.js
+++ src/components/DashboardPageContainer.js
@@ -1,3 +1,19 @@
+import { DashboardPage } from "./DashboardPage";
+
 export const DashboardPageContainer = (props) => {
-	return <div className="dashboard-page-container"></div>;
+	return (
+		<div className="dashboard-page-container">
+			<DashboardPage
+				direction={props.direction}
+				cells={props.cells}
+				visibleDashPage={props.visibleDashPage}
+				pageTitle={props.visibleDashPage}
+				acrossClueNumbers={props.acrossClueNumbers}
+				downClueNumbers={props.downClueNumbers}
+				clueProps={props.clueProps}
+				oppositeClueProps={props.oppositeClueProps}
+				onClick={props.onClick}
+			/>
+		</div>
+	);
 };
