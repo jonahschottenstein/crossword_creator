e1e84770a1a39c64b46b3be46fe1a3262bf3d7f2
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-01-28 17:50:39 -0500

    Add statement for 'Fill' condition
    Add display word which mirrors the contents of the selected word on the
    Board
    
    Add table to dynamically display the list of word matches

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,6 +1,8 @@
 import { ClueListsContainer } from "./ClueListsContainer";
 import { DashboardStatsTable } from "./DashboardStatsTable";
+import { Cell } from "./Cell";
 import { getStats } from "../utilities/stats";
+import { getWordObj } from "../utilities/words";
 
 export const DashboardPage = (props) => {
 	const {
@@ -38,6 +40,46 @@ export const DashboardPage = (props) => {
 			</div>
 		);
 	} else {
-		return <div className="dashboard-page">{props.pageTitle}</div>;
+		return (
+			<div className="dashboard-page">
+				<div className="display-word-container">
+					{getWordObj(props.direction, props.cells).selectedWordObj?.word.map(
+						(cell, index) => {
+							return (
+								<Cell
+									key={index}
+									id={`display-cell-${index}`}
+									className={"cell display-cell"}
+									index={index}
+									tabIndex={cell.tabIndex}
+									number={cell.number}
+									letter={cell.letter}
+									isSelected={cell.isSelected}
+									isBlackSquare={cell.isBlackSquare}
+								/>
+							);
+						}
+					)}
+				</div>
+				<div className="match-table-container">
+					<table className="match-table">
+						<thead>
+							<tr>
+								<th>Word</th>
+								<th>Score</th>
+							</tr>
+						</thead>
+						<tbody>
+							{props.wordMatches?.map((obj) => (
+								<tr key={obj.word} onClick={props.onMatchClick}>
+									<td className="word-match-td">{obj.word}</td>
+									<td className="word-score-td">{obj.score}</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</div>
+			</div>
+		);
 	}
 };
