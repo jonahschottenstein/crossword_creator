49b4fc0d85d703c8780ae098d44f61d2537f19ec
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-03-22 22:02:48 -0400

    Add functions to filter matches from word list

--- src/utilities/autofillGrid.js
+++ src/utilities/autofillGrid.js
@@ -14,3 +14,22 @@ const fetchWordListMemoized = async () => {
 		}
 	};
 };
+
+const isAMatch = (lettersArray, wordString) =>
+	lettersArray.every(
+		(letter, index, array) =>
+			array[index] === wordString[index] || array[index] === ""
+	);
+
+const getWordMatches = (word, sameLengthWords) => {
+	if (!word) return;
+	if (word.length < 3) return;
+	const wordLetters = word.map((cell) => cell.letter);
+	const wordMatches = sameLengthWords.filter((obj) => {
+		const testWord = obj.word;
+		if (isAMatch(wordLetters, testWord) && !/\d+/.test(testWord)) return true;
+		return false;
+	});
+
+	return wordMatches;
+};
