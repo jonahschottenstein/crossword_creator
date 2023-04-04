7f34fb38f0812020351138aa9651bc7520390f27
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-04 16:46:55 -0400

    Add methods to sort wordMatches by Scrabble score
    Words with higher scrabble scores are more likely to contain letters that are less flexible, such as Z, Q, J, and X. Because of this, sorting by Scrabble score from least to greatest makes it so the words without the less flexible letters are tried first. This, on average, should decrease the time it takes to autofill the grid

--- src/utilities/autofillGrid.js
+++ src/utilities/autofillGrid.js
@@ -297,6 +297,53 @@ const getNextWordToFill = (wordObjs) => {
 	return nextWordToFill?.wordObj;
 };
 
+const getWordMatchScrabbleScore = (wordMatch) => {
+	const letterValues = {
+		A: 1,
+		B: 3,
+		C: 3,
+		D: 2,
+		E: 1,
+		F: 4,
+		G: 2,
+		H: 4,
+		I: 1,
+		J: 8,
+		K: 5,
+		L: 1,
+		M: 3,
+		N: 1,
+		O: 1,
+		P: 3,
+		Q: 10,
+		R: 1,
+		S: 1,
+		T: 1,
+		U: 1,
+		V: 4,
+		W: 4,
+		X: 8,
+		Y: 4,
+		Z: 10,
+	};
+	const scrabbleScore = wordMatch.word
+		.split("")
+		.map((letter) => letterValues[letter])
+		.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+	return scrabbleScore;
+};
+
+const sortByScrabbleScore = (wordMatches) => {
+	const sortedWordMatches = wordMatches
+		.slice()
+		.sort(
+			(a, b) => getWordMatchScrabbleScore(a) - getWordMatchScrabbleScore(b)
+		);
+
+	return sortedWordMatches;
+};
+
 const getFilledWordObj = (wordWithMatches, matchIndex = 0) => {
 	const { wordCells, wordMatches } = wordWithMatches;
 	const wordMatch = wordMatches[matchIndex];
