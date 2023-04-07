636f70331f86149b862da2920e24dd8f731444d8
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-06 21:17:30 -0400

    Add method to erase all letters on the board

--- src/utilities/clearFill.js
+++ src/utilities/clearFill.js
@@ -1,1 +1,13 @@
+export const clearFill = (setCells) => {
+	setCells((prevState) => {
+		const newState = prevState.map((cell) => {
+			if (cell.isBlackSquare) {
+				return cell;
+			} else {
+				return { ...cell, letter: "" };
+			}
+		});
 
+		return newState;
+	});
+};
