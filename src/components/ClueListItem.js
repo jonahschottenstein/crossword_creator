2dae07e94b3c6961d8e1101ac4a320f50aef733e
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-06 22:05:42 -0400

    Refactor ClueListItem for textarea and edit button

--- src/components/ClueListItem.js
+++ src/components/ClueListItem.js
@@ -1,8 +1,24 @@
 export const ClueListItem = (props) => {
+	const clueText = props.clueListItemDataObj.clueText;
+	const editButtonClassName = props.clueListItemDataObj.editButtonClassName;
 	return (
-		<li className={props.className} onClick={props.onClick}>
+		<li className={props.className} name={props.name} onClick={props.onClick}>
 			<span className="clue-label">{props.clueLabel}</span>
-			<span className="clue-text">{props.clueText}</span>
+			{/* <span className="clue-text">{props.clueText}</span> */}
+			<span className="clue-textarea-wrapper">
+				<textarea
+					className="clue-textarea"
+					name={`${props.name}-textarea`}
+					value={clueText}
+					onChange={props.onClueLiTextareaChange}
+					onBlur={props.onClueTextareaBlur}></textarea>
+				<button
+					// className="edit-clue-button accessible"
+					className={editButtonClassName}
+					onClick={props.onClueEditButtonClick}>
+					Edit
+				</button>
+			</span>
 		</li>
 	);
 };
