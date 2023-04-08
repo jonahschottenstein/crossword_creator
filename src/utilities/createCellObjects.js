0a884808180fe51204a2a5f7d7b7a19749356a44
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-08 01:41:02 -0400

    Refactor to add clueText and getClueLabel props

--- src/utilities/createCellObjects.js
+++ src/utilities/createCellObjects.js
@@ -3,7 +3,7 @@ import { getRowAndColumn } from "../utilities/rowsColumns.js";
 const NUMBER_OF_CELLS = 225;
 const GRID_CELLS_SPAN = 15;
 
-export const createCellObjects = () => {
+/* export const createCellObjects = () => {
 	let numberedCells = [];
 	const cellsArray = Array.from(Array(NUMBER_OF_CELLS).keys()).map(
 		(key, index) => {
@@ -24,6 +24,54 @@ export const createCellObjects = () => {
 				letter: "",
 				isSelected: false,
 				isBlackSquare: false,
+			};
+		}
+	);
+	return cellsArray;
+}; */
+
+export const createCellObjects = () => {
+	let numberedCells = [];
+	const cellsArray = Array.from(Array(NUMBER_OF_CELLS).keys()).map(
+		(key, index) => {
+			if (index < GRID_CELLS_SPAN || index % GRID_CELLS_SPAN === 0) {
+				numberedCells = numberedCells.concat(key);
+			}
+			const id = `cell-${index}`;
+			const { row, column } = getRowAndColumn(key);
+			const number = numberedCells.includes(key)
+				? numberedCells.indexOf(key) + 1
+				: null;
+			const isAcross = index % GRID_CELLS_SPAN === 0;
+			const isDown = index < GRID_CELLS_SPAN;
+			const clueText = {
+				across: isAcross ? "" : undefined,
+				down: isDown ? "" : undefined,
+			};
+
+			return {
+				id,
+				index,
+				tabIndex: 0,
+				row,
+				column,
+				number,
+				across: isAcross,
+				down: isDown,
+				letter: "",
+				isSelected: false,
+				isBlackSquare: false,
+				clueText,
+				getClueLabel(direction) {
+					const dir = direction.toLowerCase();
+					if (this.isBlackSquare) return undefined;
+					if (this.number === null) return undefined;
+					if (dir !== "across" && dir !== "down") return undefined;
+					if (dir === "across" && !this.across) return undefined;
+					if (dir === "down" && !this.down) return undefined;
+
+					return `${this.number}-${dir}`;
+				},
 			};
 		}
 	);
