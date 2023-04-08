71ae6b35275d6c49c9426d5fbfb4168b59f542d9
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-08 01:45:48 -0400

    Refactor to use cells state var to make controlled

--- src/components/ClueListItem.js
+++ src/components/ClueListItem.js
@@ -1,10 +1,13 @@
 export const ClueListItem = (props) => {
-	const clueText = props.clueListItemDataObj.clueText;
-	const editButtonClassName = props.clueListItemDataObj.editButtonClassName;
+	const cells = props.cells;
+	const clueLabel = props.clueLabel;
+	const direction = props.direction;
+	const clueText = cells.find(
+		(cell) => cell.number === clueLabel && cell[direction]
+	).clueText[direction];
 	return (
 		<li className={props.className} name={props.name} onClick={props.onClick}>
 			<span className="clue-label">{props.clueLabel}</span>
-			{/* <span className="clue-text">{props.clueText}</span> */}
 			<span className="clue-textarea-wrapper">
 				<textarea
 					className="clue-textarea"
@@ -13,8 +16,8 @@ export const ClueListItem = (props) => {
 					onChange={props.onClueLiTextareaChange}
 					onBlur={props.onClueTextareaBlur}></textarea>
 				<button
-					// className="edit-clue-button accessible"
-					className={editButtonClassName}
+					className="edit-clue-button accessible"
+					// className={editButtonClassName}
 					onClick={props.onClueEditButtonClick}>
 					Edit
 				</button>
