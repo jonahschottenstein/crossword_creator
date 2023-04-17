0498f28e7812b553e6119b757cee6ac28a44b3fe
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-16 21:31:54 -0400

    Update to convert ToggleSwitches to icons

--- src/components/CellBlockSettings.js
+++ src/components/CellBlockSettings.js
@@ -6,6 +6,8 @@ export const CellBlockSettings = (props) => {
 			<ToggleSwitch
 				toggleSwitchId="cell-block-input"
 				text="Cell Block"
+				iconText="square"
+				iconClassName="material-icons"
 				name="cellBlockIsChecked"
 				isChecked={props.cellBlockIsChecked}
 				onChange={props.onChange}
@@ -13,6 +15,8 @@ export const CellBlockSettings = (props) => {
 			<ToggleSwitch
 				toggleSwitchId="symmetry-input"
 				text="Symmetry"
+				iconText="balance"
+				iconClassName="material-icons"
 				name="symmetryIsChecked"
 				isChecked={props.symmetryIsChecked}
 				onChange={props.onChange}
@@ -20,6 +24,8 @@ export const CellBlockSettings = (props) => {
 			<ToggleSwitch
 				toggleSwitchId="shaded-cell-input"
 				text="Gray"
+				iconText="square"
+				iconClassName="material-icons shaded-color"
 				name="shadedCellIsChecked"
 				isChecked={props.shadedCellIsChecked}
 				onChange={props.onChange}
@@ -27,6 +33,8 @@ export const CellBlockSettings = (props) => {
 			<ToggleSwitch
 				toggleSwitchId="circle-input"
 				text="Circle"
+				iconText="circle"
+				iconClassName="material-icons-outlined"
 				name="circleIsChecked"
 				isChecked={props.circleIsChecked}
 				onChange={props.onChange}
--- src/components/ToggleSwitch.js
+++ src/components/ToggleSwitch.js
@@ -1,4 +1,4 @@
-export const ToggleSwitch = (props) => {
+/* export const ToggleSwitch = (props) => {
 	return (
 		<div className="toggle-switch-wrapper">
 			<label htmlFor={props.toggleSwitchId} className="toggle-switch-label">
@@ -13,6 +13,26 @@ export const ToggleSwitch = (props) => {
 						onChange={props.onChange}
 					/>
 					<span className="slider round"></span>
+				</div>
+			</label>
+		</div>
+	);
+}; */
+
+export const ToggleSwitch = (props) => {
+	return (
+		<div className="toggle-switch-wrapper">
+			<label htmlFor={props.toggleSwitchId} className="toggle-switch-label">
+				<div className="toggle-switch">
+					<input
+						type="checkbox"
+						id={props.toggleSwitchId}
+						className="toggle-switch-input invisible"
+						name={props.name}
+						checked={props.isChecked}
+						onChange={props.onChange}
+					/>
+					<span className={props.iconClassName}>{props.iconText}</span>
 				</div>
 			</label>
 		</div>
