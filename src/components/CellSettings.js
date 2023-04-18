3d5e1488895136f27530c187c2e5e733a7baa966
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-18 14:57:51 -0400

    Put Toggles in div for styling/spacing

--- src/components/CellSettings.js
+++ src/components/CellSettings.js
@@ -3,42 +3,44 @@ import { ToggleSwitch } from "./ToggleSwitch.js";
 export const CellSettings = (props) => {
 	return (
 		<div className="cell-settings">
-			<ToggleSwitch
-				toggleSwitchId="symmetry-input"
-				text="Symmetry"
-				iconText="balance"
-				iconClassName="material-icons"
-				name="symmetryIsChecked"
-				isChecked={props.symmetryIsChecked}
-				onChange={props.onChange}
-			/>
-			<ToggleSwitch
-				toggleSwitchId="cell-block-input"
-				text="Cell Block"
-				iconText="square"
-				iconClassName="material-icons"
-				name="cellBlockIsChecked"
-				isChecked={props.cellBlockIsChecked}
-				onChange={props.onChange}
-			/>
-			<ToggleSwitch
-				toggleSwitchId="shaded-cell-input"
-				text="Gray"
-				iconText="square"
-				iconClassName="material-icons shaded-color"
-				name="shadedCellIsChecked"
-				isChecked={props.shadedCellIsChecked}
-				onChange={props.onChange}
-			/>
-			<ToggleSwitch
-				toggleSwitchId="circle-input"
-				text="Circle"
-				iconText="circle"
-				iconClassName="material-icons-outlined"
-				name="circleIsChecked"
-				isChecked={props.circleIsChecked}
-				onChange={props.onChange}
-			/>
+			<div className="cell-settings-toggles">
+				<ToggleSwitch
+					toggleSwitchId="symmetry-input"
+					text="Symmetry"
+					iconText="balance"
+					iconClassName="material-icons"
+					name="symmetryIsChecked"
+					isChecked={props.symmetryIsChecked}
+					onChange={props.onChange}
+				/>
+				<ToggleSwitch
+					toggleSwitchId="cell-block-input"
+					text="Cell Block"
+					iconText="square"
+					iconClassName="material-icons"
+					name="cellBlockIsChecked"
+					isChecked={props.cellBlockIsChecked}
+					onChange={props.onChange}
+				/>
+				<ToggleSwitch
+					toggleSwitchId="shaded-cell-input"
+					text="Gray"
+					iconText="square"
+					iconClassName="material-icons shaded-color"
+					name="shadedCellIsChecked"
+					isChecked={props.shadedCellIsChecked}
+					onChange={props.onChange}
+				/>
+				<ToggleSwitch
+					toggleSwitchId="circle-input"
+					text="Circle"
+					iconText="circle"
+					iconClassName="material-icons-outlined"
+					name="circleIsChecked"
+					isChecked={props.circleIsChecked}
+					onChange={props.onChange}
+				/>
+			</div>
 		</div>
 	);
 };
