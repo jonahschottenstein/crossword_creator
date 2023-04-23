32640e05b4a23c83168c3c9e74d943ee7e153e40
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-23 17:03:11 -0400

    Add method to get grid cell coordinates

--- src/utilities/getPDF.js
+++ src/utilities/getPDF.js
@@ -5,3 +5,19 @@ const getXOffset = (paperWidth = 215.9, gridWidth = 120) => {
 const getYOffset = (paperHeight = 279.4, gridHeight = 120) => {
 	return (paperHeight - gridHeight) / 2;
 };
+
+const getSquareCoords = (
+	gridLength = 15,
+	squareLength = 8,
+	xOffset = getXOffset(),
+	yOffset = getYOffset()
+) => {
+	let arr = [];
+	for (let i = 0; i < 15; i++) {
+		for (let j = 0; j <= (gridLength - 1) * squareLength; j += squareLength) {
+			arr.push({ x: j + xOffset, y: i * squareLength + yOffset });
+		}
+	}
+
+	return arr;
+};
