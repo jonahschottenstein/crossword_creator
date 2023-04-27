753a6eefdf554d00b6918cabbad04bc71c7c78af
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-27 13:45:30 -0400

    Refactor to conditionally show fill-page content

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -1,8 +1,10 @@
 import { ClueListsContainer } from "./ClueListsContainer";
 import { DashboardStatsTable } from "./DashboardStatsTable";
-import { Cell } from "./Cell";
 import { getStats } from "../utilities/stats";
 import { getWordObj } from "../utilities/words";
+import { DisplayWord } from "./DisplayWord";
+import { MatchFilter } from "./MatchFilter";
+import { MatchTable } from "./MatchTable";
 
 export const DashboardPage = (props) => {
 	const {
@@ -44,6 +46,8 @@ export const DashboardPage = (props) => {
 			</div>
 		);
 	} else {
+		const selectedWord = getWordObj(props.direction, props.cells)
+			.selectedWordObj?.word;
 		return (
 			<div className="dashboard-page fill-page">
 				<button id="clear-fill-button" onClick={props.onClearFillButtonClick}>
@@ -54,115 +58,26 @@ export const DashboardPage = (props) => {
 					onClick={props.onAutofillGridButtonClick}>
 					Autofill Grid
 				</button>
-				<DisplayWord direction={props.direction} cells={props.cells} />
-				<MatchFilter
-					matchFilterInput={props.matchFilterInput}
-					onMatchFilterChange={props.onMatchFilterChange}
-				/>
-				<MatchTable
-					wordMatches={props.wordMatches}
-					onMatchClick={props.onMatchClick}
-				/>
+
+				{!selectedWord || selectedWord.length < 3 ? (
+					<p>{"Select an entry greater than two characters to view matches"}</p>
+				) : (
+					<>
+						<DisplayWord direction={props.direction} cells={props.cells} />
+						<MatchFilter
+							matchFilterInput={props.matchFilterInput}
+							onMatchFilterChange={props.onMatchFilterChange}
+						/>
+						<MatchTable
+							wordMatches={props.wordMatches}
+							onMatchClick={props.onMatchClick}
+						/>
+					</>
+				)}
 			</div>
 		);
 	}
 };
-
-const DisplayWord = ({ direction, cells }) => {
-	return (
-		<div className="display-word-container">
-			{getWordObj(direction, cells).selectedWordObj?.word.map((cell, index) => {
-				return (
-					<Cell
-						key={index}
-						id={`display-cell-${index}`}
-						className={"cell display-cell"}
-						index={index}
-						tabIndex={cell.tabIndex}
-						number={cell.number}
-						letter={cell.letter}
-						isSelected={cell.isSelected}
-						isBlackSquare={cell.isBlackSquare}
-					/>
-				);
-			})}
-		</div>
-	);
-};
-
-const MatchFilter = ({ matchFilterInput, onMatchFilterChange }) => {
-	return (
-		<div className="match-filter-container">
-			<input
-				className="match-filter"
-				value={matchFilterInput}
-				onChange={onMatchFilterChange}></input>
-		</div>
-	);
-};
-
-const MatchTable = ({ wordMatches, onMatchClick }) => {
-	return (
-		<div className="match-table-container">
-			<table className="match-table">
-				<thead>
-					<tr>
-						<th>Word</th>
-						<th>Score</th>
-					</tr>
-				</thead>
-				<tbody>
-					{wordMatches?.current?.map((wordMatch) => (
-						<MatchRow
-							key={wordMatch.word}
-							wordMatch={wordMatch}
-							onMatchClick={onMatchClick}
-						/>
-					))}
-				</tbody>
-				{wordMatches?.hasMatchesLeft ? (
-					<MatchTableFoot onMatchClick={onMatchClick} />
-				) : null}
-			</table>
-		</div>
-	);
-};
-
-const MatchRow = ({ wordMatch, onMatchClick }) => {
-	return (
-		<tr onClick={onMatchClick}>
-			<td className="word-match-td">{wordMatch.word}</td>
-			<td className="word-score-td">{wordMatch.score}</td>
-		</tr>
-	);
-};
-
-const MatchTableFoot = ({ onMatchClick }) => {
-	return (
-		<tfoot className="match-table-foot">
-			<tr>
-				<td>
-					<button className="show-more-button" onClick={onMatchClick}>
-						Show more
-					</button>
-				</td>
-			</tr>
-		</tfoot>
-	);
-};
-
-/* 
-	const noSelectedWordMessage = "Select an entry greater than three characters to view matches";
-	return (
-		<div className="dashboard-page fill-page">
-			{}
-		
-		</div>
-	)
-	if (!selectedWordObj) {
-		return 
-	}
-*/
 
 /* 
 	conditions:
