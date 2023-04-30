8d69284bec8b2f0c7911e3f081bb1b6c58e9ecf4
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-30 16:38:20 -0400

    Refactor so each page uses same dashboard-page div

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -6,7 +6,7 @@ import { DisplayWord } from "./DisplayWord";
 import { MatchFilter } from "./MatchFilter";
 import { MatchTable } from "./MatchTable";
 
-export const DashboardPage = (props) => {
+/* export const DashboardPage = (props) => {
 	const {
 		gridSize,
 		totalWordCount,
@@ -80,6 +80,77 @@ export const DashboardPage = (props) => {
 			</div>
 		);
 	}
+}; */
+
+export const DashboardPage = (props) => {
+	const {
+		gridSize,
+		totalWordCount,
+		blackSquareCount,
+		avgWordLength,
+		scrabbleScore,
+		pangram,
+	} = getStats(props.direction, props.cells);
+	const selectedWord = getWordObj(props.direction, props.cells).selectedWordObj
+		?.word;
+	const MIN_WORD_LENGTH = 3;
+
+	return (
+		<div className={`dashboard-page ${props.visibleDashPage}-page`}>
+			{props.visibleDashPage === "stats" ? (
+				<DashboardStatsTable
+					gridSize={gridSize}
+					totalWordCount={totalWordCount}
+					blackSquareCount={blackSquareCount}
+					avgWordLength={avgWordLength}
+					scrabbleScore={scrabbleScore}
+					pangram={pangram}
+				/>
+			) : props.visibleDashPage === "clues" ? (
+				<ClueListsContainer
+					direction={props.direction}
+					cells={props.cells}
+					onClick={props.onClick}
+					onClueLiTextareaChange={props.onClueLiTextareaChange}
+					onClueEditButtonClick={props.onClueEditButtonClick}
+					onClueDoneButtonClick={props.onClueDoneButtonClick}
+					onKeyDown={props.onKeyDown}
+					onClueTextareaFocus={props.onClueTextareaFocus}
+					onClueTextareaBlur={props.onClueTextareaBlur}
+					activeTextarea={props.activeTextarea}
+				/>
+			) : (
+				<>
+					<button id="clear-fill-button" onClick={props.onClearFillButtonClick}>
+						Clear Fill
+					</button>
+					<button
+						id="autofill-grid-button"
+						onClick={props.onAutofillGridButtonClick}>
+						Autofill Grid
+					</button>
+
+					{!selectedWord || selectedWord.length < MIN_WORD_LENGTH ? (
+						<p>
+							{"Select an entry greater than two characters to view matches"}
+						</p>
+					) : (
+						<>
+							<DisplayWord direction={props.direction} cells={props.cells} />
+							<MatchFilter
+								matchFilterInput={props.matchFilterInput}
+								onMatchFilterChange={props.onMatchFilterChange}
+							/>
+							<MatchTable
+								wordMatches={props.wordMatches}
+								onMatchClick={props.onMatchClick}
+							/>
+						</>
+					)}
+				</>
+			)}
+		</div>
+	);
 };
 
 /* 
