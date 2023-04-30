8b57f3216fe555c7a76a69429de9c56e41ef6c0d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-04-29 21:15:22 -0400

    Remove unused utilities files

--- src/utilities/toggleCellBlock.js
+++
@@ -1,6 +0,0 @@
-export const toggleCellBlock = (e) => {
-	const cellBlockInput = document.querySelector("#cell-block-input");
-	if (!cellBlockInput.checked) return;
-	if (!e.target.matches("rect")) return;
-	e.target.classList.toggle("cell-block");
-};
