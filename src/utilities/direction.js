5cfc3640c018fa0a23cee668eb622ca45c805728
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 18:58:47 -0400

    Refactor fn to remove cells param for performance

--- src/utilities/direction.js
+++ src/utilities/direction.js
@@ -1,5 +1,5 @@
 // TODO: Maybe give file a more specific name
-export const setDirectionOnClick = (e, cells, setDirection) => {
+/* export const setDirectionOnClick = (e, cells, setDirection) => {
 	const selectedCellIndex = cells.findIndex((cell) => cell.isSelected);
 	const targetIndex = Number(e.target.dataset.index);
 	const selectedCellIsClicked = selectedCellIndex === targetIndex;
@@ -7,4 +7,17 @@ export const setDirectionOnClick = (e, cells, setDirection) => {
 	if (!selectedCellIsClicked) return;
 	// TODO: Maybe use changeDirection() from helpers.js
 	setDirection((d) => (d === "across" ? "down" : "across"));
+}; */
+
+import { changeDirection } from "./helpers";
+
+export const setDirectionOnClick = (e, setDirection) => {
+	const selectedCellElement = document.querySelector(".cell.selected");
+	const selectedCellIndex =
+		selectedCellElement && Number(selectedCellElement.dataset.index);
+	const targetIndex = Number(e.target.dataset.index);
+	const selectedCellIsClicked = selectedCellIndex === targetIndex;
+	if (!selectedCellIsClicked) return;
+
+	changeDirection(setDirection);
 };
