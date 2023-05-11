4c3d7e55376132c1739daf2735e9efa780c4203b
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 14:36:15 -0400

    Refactor Board w/ memo & handleKeyDown
    Modify handleKeyDown to be used for event delegation

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,6 +1,11 @@
 import { Cell } from "./Cell.js";
 import { getWordObj } from "../utilities/words.js";
 import { isInSelectedWord } from "../utilities/helpers.js";
+import { memo } from "react";
+import { handleArrowKeys } from "../utilities/arrows";
+import { handleBackspaceKey } from "../utilities/backspace";
+import { handleLetterKey } from "../utilities/letters";
+import { handleTabKey } from "../utilities/tab";
 
 const getCellClassName = (cell, selectedWord) => {
 	let className = "cell";
@@ -21,7 +26,7 @@ const getCellClassName = (cell, selectedWord) => {
 	return className;
 };
 
-export const Board = (props) => {
+/* export const Board = (props) => {
 	const { selectedWordObj } = getWordObj(props.direction, props.cells);
 	const selectedWord = selectedWordObj?.word;
 	const cells = props.cells.map((cell, index) => {
@@ -41,4 +46,44 @@ export const Board = (props) => {
 	});
 
 	return <div className="board">{cells}</div>;
-};
+}; */
+
+export const Board = memo(function Board(props) {
+	const handleKeyDown = (e) => {
+		if (!document.activeElement.matches(".cell")) return;
+
+		const direction = props.direction;
+		const setDirection = props.setDirection;
+		const cells = props.cells;
+		const setCells = props.setCells;
+
+		handleArrowKeys(e, direction, setDirection, cells);
+		handleTabKey(e, direction, setDirection, cells);
+		handleLetterKey(e, direction, setDirection, cells, setCells);
+		handleBackspaceKey(e, direction, setDirection, cells, setCells);
+	};
+
+	const { selectedWordObj } = getWordObj(props.direction, props.cells);
+	const selectedWord = selectedWordObj?.word;
+	const cells = props.cells.map((cell, index) => {
+		return (
+			<Cell
+				key={index}
+				id={`cell-${index}`}
+				className={getCellClassName(cell, selectedWord)}
+				index={index}
+				tabIndex={cell.tabIndex}
+				number={cell.number}
+				letter={cell.letter}
+				onClick={props.onClick}
+				// onKeyDown={props.onKeyDown}
+			/>
+		);
+	});
+
+	return (
+		<div className="board" onKeyDown={handleKeyDown}>
+			{cells}
+		</div>
+	);
+});
