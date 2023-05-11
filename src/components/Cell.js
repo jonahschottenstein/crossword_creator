3822133a2354cdbd69c0df8459a95b6aed4e3f36
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 18:54:57 -0400

    Add memo to Cell & remove onKeyDown prop

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -22,7 +22,10 @@
 		</div>
 	);
 }; */
-export const Cell = (props) => {
+
+import { memo } from "react";
+
+/* export const Cell = (props) => {
 	return (
 		<div
 			id={props.id}
@@ -40,6 +43,27 @@ export const Cell = (props) => {
 			</>
 		</div>
 	);
-};
+}; */
+
+export const Cell = memo(function Cell(props) {
+	return (
+		<div
+			id={props.id}
+			className={props.className}
+			data-index={props.index}
+			tabIndex={props.tabIndex}
+			onClick={props.onClick}
+			// onKeyDown={props.onKeyDown}
+		>
+			<>
+				{/circled/.test(props.className) ? (
+					<div className="circle"></div>
+				) : null}
+				<div className="number">{props.number}</div>
+				<div className="letter">{props.letter}</div>
+			</>
+		</div>
+	);
+});
 
 export default Cell;
