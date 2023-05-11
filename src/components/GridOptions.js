87247bab693308b4c500a831a1daf0ded770ac70
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 15:23:04 -0400

    Refactor GridOptions to improve performance
    Changes:
    
        - Add useCallback version of handleGridOptionClick from App.js
        - Modify setIsOpen to use callback function so the dependency array is empty

--- src/components/GridOptions.js
+++ src/components/GridOptions.js
@@ -1,6 +1,8 @@
+import { useCallback } from "react";
+import { gridOptions } from "../utilities/gridOptions";
 import { GridOption } from "./GridOption";
 
-export const GridOptions = (props) => {
+/* export const GridOptions = (props) => {
 	return (
 		<div className="grid-options">
 			{props.gridOptions.map((gridOption, index) => {
@@ -10,6 +12,29 @@ export const GridOptions = (props) => {
 						index={index}
 						gridOptionCells={gridOption}
 						onGridOptionClick={props.onGridOptionClick}
+					/>
+				);
+			})}
+		</div>
+	);
+}; */
+
+export const GridOptions = ({ setIsOpen, setCells }) => {
+	const handleGridOptionClick = useCallback((e) => {
+		const buttonIndex = e.target.name.match(/\d+$/);
+		setCells(() => gridOptions[buttonIndex]);
+		setIsOpen((prevState) => ({ ...prevState, gridOptionsPopup: false }));
+	}, []);
+
+	return (
+		<div className="grid-options">
+			{gridOptions.map((gridOption, index) => {
+				return (
+					<GridOption
+						key={index}
+						index={index}
+						gridOptionCells={gridOption}
+						onGridOptionClick={handleGridOptionClick}
 					/>
 				);
 			})}
