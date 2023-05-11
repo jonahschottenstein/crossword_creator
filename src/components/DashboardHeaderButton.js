e17a6af9890a3903a8821ca19b4d223f8547c6f5
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 18:44:42 -0400

    Add timeout to onChange to update textarea size

--- src/components/DashboardHeaderButton.js
+++ src/components/DashboardHeaderButton.js
@@ -1,3 +1,5 @@
+import { autoExpand } from "../utilities/handleClueLi";
+
 export const DashboardHeaderButton = (props) => {
 	return (
 		<label className={props.labelClassName} htmlFor={props.buttonLabel}>
@@ -8,7 +10,16 @@ export const DashboardHeaderButton = (props) => {
 				name={"dashboard-header-button"}
 				value={props.buttonLabel}
 				checked={props.visibleDashPage === props.buttonLabel}
-				onChange={props.onChange}
+				// onChange={props.onChange}
+				onChange={(e) => {
+					props.onChange(e);
+					setTimeout(() => {
+						const textareas = document.querySelectorAll(".clue-textarea");
+						textareas.forEach((textarea) => {
+							autoExpand(textarea);
+						});
+					}, 50);
+				}}
 			/>
 			{props.buttonLabel}
 		</label>
