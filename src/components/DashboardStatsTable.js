73abca134e479cb3c118fa833639dad4d454125b
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 15:31:36 -0400

    Refactor with memo & individual props

--- src/components/DashboardStatsTable.js
+++ src/components/DashboardStatsTable.js
@@ -1,4 +1,4 @@
-export const DashboardStatsTable = (props) => {
+/* export const DashboardStatsTable = (props) => {
 	//? Should <tr>s and/or <td>s be components?
 	return (
 		<table className="dashboard-stats-table">
@@ -30,4 +30,46 @@ export const DashboardStatsTable = (props) => {
 			</tbody>
 		</table>
 	);
-};
+}; */
+
+import { memo } from "react";
+
+export const DashboardStatsTable = memo(function DashboardStatsTable({
+	gridSize,
+	totalWordCount,
+	blackSquareCount,
+	avgWordLength,
+	scrabbleScore,
+	pangram,
+}) {
+	return (
+		<table className="dashboard-stats-table">
+			<tbody>
+				<tr>
+					<td className="td-label">Size:</td>
+					<td className="td-value">{gridSize}</td>
+				</tr>
+				<tr>
+					<td className="td-label">Total Words:</td>
+					<td className="td-value">{totalWordCount}</td>
+				</tr>
+				<tr>
+					<td className="td-label">Black Squares:</td>
+					<td className="td-value">{blackSquareCount}</td>
+				</tr>
+				<tr>
+					<td className="td-label">Avg. Word Length:</td>
+					<td className="td-value">{avgWordLength}</td>
+				</tr>
+				<tr>
+					<td className="td-label">Scrabble score:</td>
+					<td className="td-value">{scrabbleScore}</td>
+				</tr>
+				<tr>
+					<td className="td-label">Pangram:</td>
+					<td className="td-value">{pangram}</td>
+				</tr>
+			</tbody>
+		</table>
+	);
+});
