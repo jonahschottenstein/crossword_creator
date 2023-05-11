acea660a7d87df875f540a68e467ea63c8ac6b13
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-11 19:00:10 -0400

    Refactor fn to remove cells param for performance

--- src/utilities/setSymmetricalCellStyle.js
+++ src/utilities/setSymmetricalCellStyle.js
@@ -1,7 +1,23 @@
-export const setSymmetricalCellStyle = (e, cells, setCells, setFn) => {
+/* export const setSymmetricalCellStyle = (e, cells, setCells, setFn) => {
 	const numberOfCells = cells.length;
 	const lastCellIndex = numberOfCells - 1;
 	const centerCellIndex = Math.floor(numberOfCells / 2);
+	const targetIndex = Number(e.target.dataset.index);
+	const symmetricalCellIndex = lastCellIndex - targetIndex;
+
+	if (targetIndex !== centerCellIndex) {
+		setFn(e, setCells, symmetricalCellIndex);
+	}
+}; */
+
+export const setSymmetricalCellStyle = (
+	e,
+	setCells,
+	setFn,
+	cellsCount = 225
+) => {
+	const lastCellIndex = cellsCount - 1;
+	const centerCellIndex = Math.floor(cellsCount / 2);
 	const targetIndex = Number(e.target.dataset.index);
 	const symmetricalCellIndex = lastCellIndex - targetIndex;
 
