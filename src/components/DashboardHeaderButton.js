3d9b6b6b0b86cdfb83def3f9c743abd0245d217d
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-05-13 17:45:21 -0400

    Destructure props & extract onChange inline func

--- src/components/DashboardHeaderButton.js
+++ src/components/DashboardHeaderButton.js
@@ -1,27 +1,32 @@
 import { autoExpand } from "../utilities/handleClueLi";
 
-export const DashboardHeaderButton = (props) => {
-	// TODO: Maybe make onChange a function expression
+export const DashboardHeaderButton = ({
+	buttonLabel,
+	labelClassName,
+	visibleDashPage,
+	onChange,
+}) => {
+	const handleAutoexpandTextareas = (e) => {
+		onChange(e);
+		setTimeout(() => {
+			const textareas = document.querySelectorAll(".clue-textarea");
+			textareas.forEach((textarea) => {
+				autoExpand(textarea);
+			});
+		}, 50);
+	};
 	return (
-		<label className={props.labelClassName} htmlFor={props.buttonLabel}>
+		<label className={labelClassName} htmlFor={buttonLabel}>
 			<input
 				type={"radio"}
-				id={props.buttonLabel}
+				id={buttonLabel}
 				className={"dashboard-header-button"}
 				name={"dashboard-header-button"}
-				value={props.buttonLabel}
-				checked={props.visibleDashPage === props.buttonLabel}
-				onChange={(e) => {
-					props.onChange(e);
-					setTimeout(() => {
-						const textareas = document.querySelectorAll(".clue-textarea");
-						textareas.forEach((textarea) => {
-							autoExpand(textarea);
-						});
-					}, 50);
-				}}
+				value={buttonLabel}
+				checked={visibleDashPage === buttonLabel}
+				onChange={handleAutoexpandTextareas}
 			/>
-			{props.buttonLabel}
+			{buttonLabel}
 		</label>
 	);
 };
