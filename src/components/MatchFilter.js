5e5c9af96c2c34523ecb3f31f59fa9b96e1cdf40
Jonah Schottenstein <jonahschottenstein@gmail.com>
2023-09-27 16:39:53 -0600

    Add MatchFilter label

--- src/components/MatchFilter.js
+++ src/components/MatchFilter.js
@@ -1,7 +1,9 @@
 export const MatchFilter = ({ matchFilterInput, onMatchFilterChange }) => {
 	return (
 		<div className="match-filter-container">
+			<label htmlFor="match-filter-input">Regular Expression Filter</label>
 			<input
+				id="match-filter-input"
 				className="match-filter"
 				value={matchFilterInput}
 				onChange={onMatchFilterChange}></input>
