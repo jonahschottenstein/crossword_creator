c3f6defe498215caf03bc3ad3aed4a49cb6bb963
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-02-23 20:07:38 -0700

    Fix handleSpaceKey to work with MobileKeyboard

--- src/utilities/space.js
+++ src/utilities/space.js
@@ -1,7 +1,12 @@
 export const handleSpaceKey = (e, direction, setDirection) => {
 	e.preventDefault();
 
-	if (e.key !== " ") return;
+	if (e.type === "keydown" && e.key !== " ") return;
+	if (
+		e.type === "click" &&
+		!e.target.matches("button.mobile-keyboard-key[value='_']")
+	)
+		return;
 
 	const newDirection = direction === "across" ? "down" : "across";
 
