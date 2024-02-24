8aad6ec66305083a0f31421fed48214775e4c3fc
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-02-23 19:12:45 -0700

    Update setCellLetter to work with MobileKeyboard

--- src/utilities/setCellLetter.js
+++ src/utilities/setCellLetter.js
@@ -2,11 +2,13 @@ import { getSelectedCell } from "./helpers";
 
 export const setCellLetter = (e, cells, setCells) => {
 	const selectedCell = getSelectedCell(cells);
+	const letterKeyIsPressed = e.target.matches("button.mobile-keyboard-key");
+	const letter = letterKeyIsPressed ? e.target.value : e.key.toUpperCase();
 
 	setCells((prevState) => {
 		const newState = prevState.map((cell) => {
 			if (cell.index === selectedCell.index) {
-				return { ...cell, letter: e.key.toUpperCase() };
+				return { ...cell, letter };
 			} else {
 				return cell;
 			}
