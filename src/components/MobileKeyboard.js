a6114d6afa79dcd7e3443769f5cc70046a1d5783
Jonah Schottenstein <jonahschottenstein@gmail.com>
2024-02-26 19:46:46 -0700

    Change MobileKeyboard layout & use backspace icon
    Change 'Backspace' to 'backspace' for material-icons icon
    
    Remove fourth row of MobileKeyboard (including Tab and Space keys), and
    put backspace key in third row. The Tab and Space keys are unnecessary
    for mobile users, and having a fourth row made the keys smaller.

--- src/components/MobileKeyboard.js
+++ src/components/MobileKeyboard.js
@@ -50,8 +50,7 @@ export const MobileKeyboard = ({
 	const rows = [
 		["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
 		["A", "S", "D", "F", "G", "H", "J", "K", "L"],
-		["Z", "X", "C", "V", "B", "N", "M"],
-		["Tab", "_", "Backspace"],
+		["Z", "X", "C", "V", "B", "N", "M", "backspace"],
 	];
 	return (
 		<div className="mobile-keyboard">
